72359401282996f7c2a85391d1dc54ea91cef45a
elephantproject <elephantproject@protonmail.com>
2021-11-02 17:38:07 -0700

    1.7.1

--- src/buildList.js
+++ src/buildList.js
@@ -4,8 +4,8 @@ const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
 const kovan = require("./tokens/kovan.json");*/
-const bscMainnet = require("./tokens/bsc-mainnet.json");
-const bscTestnet = require("./tokens/bsc-testnet.json");
+// const bscMainnet = require("./tokens/bsc-mainnet.json");
+// const bscTestnet = require("./tokens/bsc-testnet.json");
 const harmonyMainnet = require("./tokens/harmony-mainnet.json");
 const harmonyTestnet = require("./tokens/harmony-testnet.json");
 
@@ -20,9 +20,9 @@ module.exports = function buildList() {
       patch: +parsed[2],
     },
     tags: {},
-    logoURI: "https://dvwecb5klcqus.cloudfront.net/venomswap/logos/venomswap-128x128.png",
-    keywords: ["elephant", "default"],
-    tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet]
+    logoURI: "https://elephantdefi.s3.us-west-1.amazonaws.com/ELEPHANT.png",
+    keywords: ["elephant", "default", "token", "list"],
+    tokens: [...harmonyMainnet, ...harmonyTestnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
